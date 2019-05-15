e4048551cda101352699e98238fa6f9cc4f8e0ca
Chris Guy <christien.guy@gmail.com>
2019-05-15 08:42:23 +0100

    Tidy up unused files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import useClickAway from "react-click-away";
+import "./App.css";
 
 const App: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(true);
+  const [ref] = useClickAway(() => {
+    setMenuOpen(false);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="menu-button" onClick={() => setMenuOpen(!menuOpen)}>
+        Toggle Menu
+      </button>
+
+      {menuOpen ? (
+        <div className="menu" ref={ref}>
+          <p>Inside here you might put some options that have low importance</p>
+        </div>
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
